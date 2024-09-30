118f57b801b2db2b8a2b0f403f9f01c92272b031
Raffaela <raffa.fdo@gmail.com>
2024-09-30 19:12:49 -0300

    atualizado

--- 270924/desafio.js
+++ 270924/desafio.js
@@ -1,8 +1,9 @@
 let valor1 = prompt("digite um valor")
 let valor2 = prompt('digite outro valor')
 
-
-let x = valor1 + valor2 // nao consegui mudar de string pra numero utilizando number :) dai parei 
+let num1 = Number(valor1)
+let num2 = Number(valor2)
+let x = num1 + num2
 let y = valor1 * valor2
 
 console.log(`O primeiro número(${valor1}) somado ao segundo número(${valor2}) resulta em: ${x}.  O primeiro número(${valor1}) multiplicado pelo segundo número(${valor2}) resulta em: ${y}`)
